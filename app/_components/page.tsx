66c4c448b5b8904390e5246bf4571206d882b568
SoumyadeepOSD <opencontainer255@gmail.com>
2024-03-01 21:49:38 +0530

    hint over hover implemented

--- app/_components/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Page = () => {
-  return (
-    <div>I am never rendered</div>
-  )
-}
-
-export default Page
--- app/users/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const page = () => {
-  return (
-    <div>I am just a normal page inside users folder</div>
-  )
-}
-
-export default page
