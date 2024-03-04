7bfaecc78421945a1b2981df8cb21f22d88f4fcb
SoumyadeepOSD <opencontainer255@gmail.com>
2024-03-05 01:53:41 +0530

    template of no-org created

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,9 @@
+import EmptyOrg from "./_components/empty-org";
+
 const DashboardPage = () => {
     return(
-        <div>
-            Dashboard Root page
+        <div className="flex-1 h-[calc(100%-60px)] p-6">
+            <EmptyOrg/>
         </div>
     );
 }
